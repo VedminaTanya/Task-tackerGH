f07543a2aa8340b0cbc4a78385a7afb72c8c58ee
VedminaTanya <Tanya_Norilsk@mail.ru>
2016-08-01 20:45:26 +0300

    Динамическое создание панели

---
+++ common_board.js
@@ -0,0 +1,130 @@
+/**
+ * Created by Сестрички on 29.07.2016.
+ */
+var requestExample = {
+    _id: "kgktkf4443fjfh",
+    assigned_to_user_login: "test_user",
+    name: "Create some front end",
+    task_text: "Implement this",
+    expiration_date: "1467994183",
+    additional_files: [],
+    urgency: "normal",
+    task_status: "assigned"
+};
+//
+// function createTask(obj) {
+//
+//     var maskOfTask = $('<article/>', {
+//         id: obj._id,
+//         class: 'col-xs-12 col-sm-6 col-md-4 expanding-content-container'
+//     }).append(
+//         $('<div/>', {
+//             class: 'panel panel-default'
+//         }).append(
+//             $('<header/>', {
+//                     class: 'panel-heading'
+//                 }.append(
+//                 $('<h4/>', {
+//                     text: obj.name
+//                 }).append(
+//                     $('<span/>', {
+//                         class: 'label label-default label-as-badge pull-right',
+//                         // data-toggle:  'popover',
+//                         text: obj.task_status
+//                     })
+//                 )
+//                 )
+// //                 $('<div/>', {
+// //         class: 'panel-body'
+// //     }).append(
+// //         $('<p/>', {
+// //             id: 'text' + obj._id,
+// //             class: 'expanding-text'
+// //         }),
+// //         $('<a/>', {
+// //             class: 'seeMore',
+// //             href: '#text' + obj._id,
+// //             text: 'see more...'
+// //         }),
+// //         $('<a/>', {
+// //             class: 'rollUp',
+// //             href: '#',
+// //             text: 'roll up...'
+// //         })
+// //     )
+// // ),
+// //     $('<footer/>', {
+// //         class: 'panel-footer'
+// //
+// //     }).append(
+// //         $('<div/>', {
+// //             class: 'btn-group btn-group-justified'
+// //         }).append(
+// //             $('<div/>', {
+// //                 class: 'btn-group',
+// //                 role: 'group'
+// //             }).append(
+// //                 $('<button/>', {
+// //                     class: 'btn btn-success',
+// //                     text: 'Finish'
+// //                 }).append(
+// //                     $('<i/>', {
+// //                         class: 'glyphicon glyphicon-ok'
+// //                     })
+// //                 )
+// //             ),
+// //             $('<div/>', {
+// //                 class: 'btn-group btn-group-justified'
+// //             }).append(
+// //                 $('<div/>', {
+// //                     class: 'btn-group',
+// //                     role: 'group'
+// //                 }).append(
+// //                     $('<button/>', {
+// //                         class: 'btn btn-danger',
+// //                         text: 'Discard'
+// //                     }).append(
+// //                         $('<i/>', {
+// //                             class: 'glyphicon glyphicon glyphicon-remove'
+// //                         })
+// //                     )
+// //                 )
+//                     )
+//                 )
+//            // )
+//         )
+// }
+// return maskOfTask;
+// }
+//
+// createTask(requestExample);
+
+function createTask(obj) {
+    var maskOfTask = $('<article/>', {
+        id: obj._id,
+        class: 'col-xs-12 col-sm-6 col-md-4 expanding-content-container'
+    }).append(
+        $('<div/>', {
+            class: 'panel panel-default'
+        }).append(
+            $('<header/>', {
+                class: 'panel-heading'
+            }).append(
+                $('<h4/>', {
+                    text: obj.name
+                }).append(
+                    $('<span/>', {
+                        class: 'label label-default label-as-badge pull-right',
+                        // data-toggle:  'popover',
+                        text: obj.task_status
+                    })
+                )
+            )
+        )
+    )
+
+
+    return maskOfTask;
+}
+
+createTask(requestExample);
